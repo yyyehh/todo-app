6abe0bc4d1e232d98cd89fcc12f3c2a32f765487
yyyehh <414572043@m365.fju.edu.tw>
2026-09-24 22:46:34 +0800

    Day 2：拆解Request並印出內容

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    console.log('請求方法：', req.method);
+    console.log('請求網址：', req.url);
+    console.log('請求標頭：', req.headers);
+
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Todo App!');
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end('收到請求了');
 });
 
 server.listen(3000, () => {
